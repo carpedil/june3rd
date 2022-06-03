ce80472790ab8bd494e989cf26e08913e58064df
carpedil <cir.yan.dev@outlook.com>
2022-06-03 09:49:21 +0800

    第一个类组建

---
+++ src/01-base/01_ClassComponent.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>This is my App</div>
+    )
+  }
+}
+
+/* class Test {
+  constructor(){
+    this.a = 1
+    this.b = 2
+  }
+
+  testa(){
+    console.log("testa")
+  }
+  testb(){
+    console.log("testb")
+  }
+}
+
+class Child extends Test {
+  constructor(){
+    this.c = 1
+  }
+
+  testc(){
+    console.log("testc")
+  }
+} */
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import App from './App'
+/* import App from './App' */
+import App from './01-base/01_ClassComponent'
 
 ReactDOM.render(<App></App>,document.getElementById('root'))
