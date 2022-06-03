88e862acee4efed11fb8c3e5012c6c2a6efc91c1
carpedil <cir.yan.dev@outlook.com>
2022-06-03 10:09:20 +0800

    函数式组件

---
+++ src/01-base/02_FunctionalComponent.js
@@ -0,0 +1,16 @@
+/* 安装ES7插件后, 输入rfc即可快速创建函数式组件代码片段 */
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+            <div>This is a Functional Component</div>
+            <ul>
+                <li>为什么学习React</li>
+                <li>我要跳槽</li>
+                <li>我要赚钱</li>
+            </ul>
+    </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 /* import App from './App' */
-import App from './01-base/01_ClassComponent'
+/* import App from './01-base/01_ClassComponent' */
+import App from './01-base/02_FunctionalComponent'
 
 ReactDOM.render(<App></App>,document.getElementById('root'))
