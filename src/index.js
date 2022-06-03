ede75cd9aec84d0a9e69d49c81f1ff8ad8895734
carpedil <cir.yan.dev@outlook.com>
2022-06-03 11:44:56 +0800

    ...

--- src/index.js
+++ src/index.js
@@ -8,4 +8,4 @@ import ReactDOM from 'react-dom'
 /* import App from './01-base/05_事件绑定1' */
 import App from './01-base/07_事件绑定3'
 
-ReactDOM.render(<App></App>,document.getElementById('root'))
+ReactDOM.render(<App/>,document.getElementById('root'))
