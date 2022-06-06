ed000e18e125bdd05e9367679ea93d8a99a3b6b9
carpedil <cir.yan.dev@outlook.com>
2022-06-06 21:43:35 +0800

    兄弟组件通信-发布订阅模式

--- src/02_advanced/07_发布订阅模式.js
+++ src/02_advanced/07_发布订阅模式.js
@@ -49,7 +49,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>中间人模式</h1>
+                <h1>发布订阅模式</h1>
                 {
                     this.state.filmList.map(item => {
                         return <FilmItem key={item.filmId} {...item}></FilmItem>
