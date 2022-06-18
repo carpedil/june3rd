a09e3666897e1bd0293ffe76b1f1ab7ebe4b6e79
carpedil <cir.yan.dev@outlook.com>
2022-06-18 20:20:39 +0800

    嵌套路由

---
+++ src/04_router/03_嵌套路由.js
@@ -0,0 +1,16 @@
+import { Component } from 'react'
+import IndexRouter from './index/indexrouter3'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <p>App</p>
+                <div>
+                    {/* 其他内容 */}
+                   <IndexRouter/>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/04_router/views/films/NowPlaying.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class NowPlaying extends Component {
+  render() {
+    return (
+      <div>NowPlaying</div>
+    )
+  }
+}
